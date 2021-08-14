daf8fd2b486898be6d9dcb608fbeae436fe21bb3
Mark <mark@example.dev>
2021-08-14 00:14:29 -0700

    home view

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Pet Adoption App"""
 
 from flask import Flask, redirect, render_template, request, flash
-from models import db, connect_db
+from models import db, connect_db, Pet
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'YOUR_KEY_HERE'
@@ -10,3 +10,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
+@app.route('/')
+def home_view():
+    pets = Pet.query.all()
+    return render_template('home.html', pets = pets)
